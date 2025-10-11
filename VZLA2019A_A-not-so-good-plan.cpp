b1441ea2c93587ce4499e3536ddad66aa64e340f
MagiCat <corinavicky07@gmail.com>
2025-10-10 21:44:52 -0400

    Update VZLA2019A_A-not-so-good-plan.cpp

--- VZLA2019A_A-not-so-good-plan.cpp
+++ VZLA2019A_A-not-so-good-plan.cpp
@@ -1,27 +1,3 @@
-/*
-
-VZLA2019A - A not so good plan
-no tags 
-
-On the International Conglomerate of Pest Control (ICPC) they make experiments with exactly five laboratory mice. They are numbered from 1 to 5 and they are extremely intelligent
-
-Last night they developed a escape plan, but surprisingly only one mouse made it out of the building. Can you tell which one escaped?
-
-Input
-The first and only line contains exactly four different integers a, b, c, d, being the number of each mouse remaining on the laboratory respectively..
-
-Output
-Print the number of the mouse that escaped
-
-Example
-Input:
-1 2 4 5
-
-Output:
-3
-Constraints
-1 ≤ a, b, c, d ≤ 5
-*/
 #include <iostream>
 #include <numeric>
 #include <vector>
